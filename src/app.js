58542fd49b21bac8be1a078c170671e9e30a90f0
Kalvium-Program <109285740+Kalvium-Program@users.noreply.github.com>
2022-12-02 15:07:37 +0530

    5 files modified, 1 deleted

--- src/app.js
+++ src/app.js
@@ -2,45 +2,35 @@
 const NEG_INF = -1000000;
 
 function createPop(givenArray, item) {
-
-  // Progression 2: add 3 variables: title, currIndex, check
-  const title = document.createElement('h3');
+  // Progression 2: initialize 2 variables: currIndex, check
   let currIndex = NEG_INF;
   let check = false;
-  document.getElementById('root').append(title);
 
-  // Progression 3: create a function "searchForElement" 
-  function searchForElement(array, givenItem){
-    array.forEach((element,index) => {
-      if(element==givenItem){
-        currIndex=index;
+  // Progression 3: create a function "searchForElement"
+  function searchForElement(array, givenItem) {
+    array.forEach((element, index) => {
+      if (element == givenItem) {
+        currIndex = index;
         check = true;
       }
     });
   }
 
   // Progression 4: In `createPop()`, return a function - which uses the updated values of `currIndex` and `check`
-                    // to return the desired output.
-  return function(){
-
-    searchForElement(givenArray,item);
+  // to return the desired output.
+  return function () {
+    searchForElement(givenArray, item);
     let index = currIndex;
     let isPresent = check;
 
-    if(isPresent){
-      title.innerHTML+=`The item is present and is at index ${index}`;
+    if (isPresent) {
       return `The item is present and is at index ${index}`;
-    }
-    else{
-      title.innerHTML+=`The item is not present and is at index ${index}`;
+    } else {
       return `The item is not present and is at index ${index}`;
     }
-
   };
 }
-
-const arrayOfNumbers = [1,2,3,4,5,6];
+const arrayOfNumbers = [1, 2, 3, 4, 5, 6];
 const itemToSearch = 4;
-const pop = createPop(arrayOfNumbers,itemToSearch);
+const pop = createPop(arrayOfNumbers, itemToSearch);
 pop();
-
